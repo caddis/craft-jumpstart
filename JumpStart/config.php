869a52d580e2bbff2848f7e143a7db77fa72682e
Taylor Daughtry <hello@taylordaughtry.com>
2016-06-10 22:44:17 -0500

    Initial commit

---
+++ JumpStart/config.php
@@ -0,0 +1,8 @@
+<?php
+
+	// See the README for notes on how this works. This file is also totally optional.
+
+return array(
+    'firstSample' => 'firstValue',
+    'secondSample' => 'secondValue'
+);
